fd735a3cda8921bf30a828bb58446c0efdbf4d5b
Agustin Carrizo <agustin.carrizo@sovos.com>
2023-08-15 20:08:53 -0300

    Working model with data.csv

--- Python_Challenges/File_System/Python-Replace-String-in-File-Directory/python-Replace-String-in-File-Directory.py
+++
@@ -1,6 +0,0 @@
-import os
-
-directory = '/home/paul/Pictures'
-
-[os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('some-string', 'other-string')) for f in os.listdir(directory)]
-
--- Statistics/Standard-Deviation/standard-deviation.md
+++
@@ -1,7 +0,0 @@
-# Youtube Link => https://youtu.be/w2WrskWX60o
-
-![](assets/2020-08-08-21-34-57.png)
-
-Now an example
-
-![](assets/2020-08-08-21-33-14.png)
